696104ee001ccc3ec9ef8e7cf6be97f748a2064e
shuhei.takai <stst.shu82@gmail.com>
2019-09-15 20:15:10 +0900

    メール送信機能を付けた

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,12 @@
 class TasksController < ApplicationController
+before_action :set_task, only: [:show, :edit, :update, :destroy]
+
   def index
-    @task = Task.all
+    @q = current_user.tasks.ransack(params[:q])
+    @tasks = @q.result(distinct: true)
   end
 
   def show
-    @task = Task.find(params[:id])
   end
 
   def new
@@ -12,30 +14,49 @@ class TasksController < ApplicationController
   end
 
   def edit
-    @task = Task.find(params[:id])
   end
 
   def create
-    @task = Task.new(task_params)
-    @task.save!
-    redirect_to tasks_url, notice: "タスク「#{@task.name}」を登録しました。"
+    @task = current_user.tasks.new(task_params)
+
+    if params[:back].present?
+      render :new
+      return
+    end
+
+    if @task.save
+      TaskMailer.creation_email(@task).deliver_now
+      redirect_to tasks_url, notice: "タスク「#{@task.name}」を登録しました。"
+    else
+      render :new
+    end
   end
 
   def update
-    task = Task.find(params[:id])
-    task.update!(task_params)
-    redirect_to tasks_url, notice: "タスク「#{task.name}」を更新しました。"
+    if @task.update!(task_params)
+      redirect_to tasks_url, notice: "タスク「#{@task.name}」を更新しました。"
+    else
+      render :edit
+    end
   end
 
   def destroy
-    @task = Task.find(params[:id])
     @task.destroy
     redirect_to tasks_url, notice:"タスク「#{@task.name}」を削除しました。"
+  end
+
+  def confirm_new
+    @task = current_user.tasks.new(task_params)
+    render :new unless @task.valid?
   end
 
   private
 
   def task_params
     params.require(:task).permit(:name, :description)
+  end
+
+  def set_task
+    @task = current_user.tasks.find(params[:id])
   end
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,22 @@
 class Task < ApplicationRecord
+  validates :name, presence: true, length: { maximum: 30 }
+  validate :validate_name_not_null_including_colmma
+
+  belongs_to :user
+
+  scope :recent, -> { order(created_at: :desc) }
+
+  private
+
+  def validate_name_not_null_including_colmma
+    errors.add(:name, 'にカンマを含めることはできません') if name&.include?(',')
+  end
+
+  def self.ransackable_attributes(auth_object = nil)
+    %w[name created_at]
+  end
+
+  def self.ransackable_associations(auth_object = nil)
+    []
+  end
 end
